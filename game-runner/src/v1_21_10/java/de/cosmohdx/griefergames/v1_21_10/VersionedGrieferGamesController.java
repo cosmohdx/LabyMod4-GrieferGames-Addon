bfa10ef8c273a4cab3612bfbf75302e348c42059
christianlinde <info@christian-linde.de>
2026-09-25 10:04:19 +0200

    start complete remaster

---
+++ game-runner/src/v1_21_10/java/de/cosmohdx/griefergames/v1_21_10/VersionedGrieferGamesController.java
@@ -0,0 +1,40 @@
+package de.cosmohdx.griefergames.v1_21_10;
+
+import de.cosmohdx.griefergames.core.GrieferGamesController;
+import net.labymod.api.models.Implements;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+@Implements(GrieferGamesController.class)
+public class VersionedGrieferGamesController extends GrieferGamesController {
+
+  @Inject
+  public VersionedGrieferGamesController() {}
+
+  @Override
+  public boolean playerAllowedFlying() {
+    if (Minecraft.getInstance().player == null) {
+      return false;
+    }
+    return Minecraft.getInstance().player.getAbilities().mayfly;
+  }
+
+  @Override
+  public boolean hideBoosterMenu() {
+    if (Minecraft.getInstance().player == null) {
+      return false;
+    }
+    Screen screen = Minecraft.getInstance().screen;
+    if (screen instanceof AbstractContainerScreen) {
+      if (screen.getTitle().getString().equals("§6Booster - Übersicht")) {
+        Minecraft.getInstance().player.closeContainer();
+        return true;
+      }
+    }
+    return false;
+  }
+}
---
+++ game-runner/src/v26_3/java/de/cosmohdx/griefergames/v26_3/VersionedGrieferGamesController.java
@@ -0,0 +1,42 @@
+package de.cosmohdx.griefergames.v26_3;
+
+import de.cosmohdx.griefergames.core.GrieferGamesController;
+import net.labymod.api.Laby;
+import net.labymod.api.models.Implements;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+@Implements(GrieferGamesController.class)
+public class VersionedGrieferGamesController extends GrieferGamesController {
+
+  @Inject
+  public VersionedGrieferGamesController() {}
+
+  @Override
+  public boolean playerAllowedFlying() {
+    if (Minecraft.getInstance().player == null) {
+      return false;
+    }
+    return Minecraft.getInstance().player.getAbilities().mayfly;
+  }
+
+  @Override
+  public boolean hideBoosterMenu() {
+    if (Minecraft.getInstance().player == null) {
+      return false;
+    }
+    Object rawScreen = Laby.labyAPI().minecraft().minecraftWindow().getCurrentVersionedScreen();
+    if (rawScreen instanceof AbstractContainerScreen) {
+      Screen screen = (Screen) rawScreen;
+      if (screen.getTitle().getString().equals("§6Booster - Übersicht")) {
+        Minecraft.getInstance().player.closeContainer();
+        return true;
+      }
+    }
+    return false;
+  }
+}
